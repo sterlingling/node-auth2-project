ea88a00745b312a1462c0cb3142cd9fdff5b8bfa
Sterling Sanders <sterlingsanders723@gmail.com>
2024-02-05 21:21:13 -0800

    done

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,18 @@ function find() {
       }
     ]
    */
+  /*
+  select
+    user_id,
+    username,
+    role_name
+from users
+    join roles on
+users.role_id = roles.role_id
+  */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +46,21 @@ function findBy(filter) {
       }
     ]
    */
+  /* 
+  select
+    user_id,
+    username,
+    password,
+    role_name
+from users
+    join roles on
+users.role_id = roles.role_id
+where users.user_id = 1
+  */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +74,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
